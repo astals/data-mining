5fdb08695005351f07bb4b91379e3ae92227fd3c
Master ciberseguridad 2 <master_cs18@a5s104mac54.fib.upc.es>
2016-04-21 21:21:22 +0200

    voy a matar a alguien

--- R/plots.R
+++ R/plots.R
@@ -6,8 +6,17 @@ networkPlot <- function(input_data){
 }
 
 severityPlot <- function(input_data){
-  input_data <- runif(3000, min=0, max=10)
-  bar_data <- table(floor(input_data))
+  input_data <- runif(5, min=0, max=10)
+  input_data <- floor(input_data)
+  bar_data <- matrix(ncol= 11, nrow = 2, data=rbind(0:10,+rep(0,11)))
+  l=list()
+  colnames<-c('0','1','2','3', '4', '5', '6', '7','8','9','10')
+  for (i in list(0:10)[1]){
+    append(l,length(which(input_data==i )))
+  }
   print(bar_data)
-  barplot(bar_data, main="Vulnerability Severity Distribution",ylab="count", xlab = "severity")
+  paleta <- colorRampPalette(c("yellow","red"))
+  colors <- paleta(10)
+  colname
+  barplot(l, main="Vulnerability Severity Distribution", names.arg=colnames, col=colors,ylab="count", xlab = "severity")
 }
